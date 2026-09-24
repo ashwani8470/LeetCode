05162180254426126552c9e3ba4414c56929c422
Ashwani kumar singh <ashwanisingh8470@gmail.com>
2026-09-24 18:54:59 +0530

    Time: 14 ms (92.29%), Space: 43.5 MB (70.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,3 +1,6 @@
+
+    //Approach 1 5%
+    /*
 class Solution {
     private int palendrome(String a){
         int n1=a.length();
@@ -30,5 +33,43 @@ class Solution {
         }
         return a;
 
+    }
+}*/
+
+//Approach 2
+class Solution {
+
+    private int expand(String s, int left, int right) {
+
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    public String longestPalindrome(String s) {
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start + 1) {
+
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
     }
 }
